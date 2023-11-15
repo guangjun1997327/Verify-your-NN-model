706859cc18f33022efd409362458eafedc6795f6
guangjun1997327 <115068545+guangjun1997327@users.noreply.github.com>
2023-11-15 11:15:43 +0100

    Update Synthetic_Regression.py

--- model-capability-test/Regression/Synthetic_Regression.py
+++ model-capability-test/Regression/Synthetic_Regression.py
@@ -1,15 +1,5 @@
 import json, sys
-# sys.path.append('models/PBP_net/')
-# import PBP_net
-from TAGI.BayesNNactfunction import Bayesian_Network_torch as Bayesian_Network
-# from meanfield import Bayesian_Network_torch as Bayesian_Network
-# from tagireproduce import Bayesian_Network_torch as Bayesian_Network
-# from models.BayesNN import Bayesian_Network_torch as Bayesian_Network
-from helper_functions.aux_function import *
-from helper_functions.plots import plot_synthetic_plot
-from models.Dropout_yaringal import net
-from numpy.random import seed
-seed(1)
+from model.yourNN import NN_torch as NN
 import tensorflow as tf
 tf.random.set_seed(2)
 tf.get_logger().setLevel('ERROR')
@@ -64,41 +54,27 @@ def run_regression(ds, size, epochs, trials, hidden):
         # m, v, v_noise = pbp.predict(X_test)
         # rmse_pbp[t], nll_pbp[t] = rmse_regression(y_test, m, v+v_noise)
 
-        bnn = Bayesian_Network(layers, activations, load_from_keras=False, input_scaler=X_train_scaler, output_scaler=y_train_scaler, verbose=False)
+        nn = Network(layers, activations, load_from_keras=False, input_scaler=X_train_scaler, output_scaler=y_train_scaler, verbose=False)
 
         X_train, y_train = torch.from_numpy(X_train).float(), torch.from_numpy(y_train).float()
         X_test, y_test = torch.from_numpy(X_test).float(), torch.from_numpy(y_test).float()
 
-        print("Train KBNN")
+        print("Train NN")
         start = time.time()
         for e in range(epochs):
             bnn.train(X_train, y_train)
         end = time.time()
-        train_times_kbnn[t] = end - start
+        train_times[t] = end - start
         y_pred, y_cov = bnn.predict(X_test)
-        rmse_kbnn[t], nll_kbnn[t] = rmse_regression(y_test, y_pred, y_cov)
+        rmse[t], nll[t] = rmse_regression(y_test, y_pred, y_cov)
 
-    # rmse_mc_mean, rmse_mc_std = np.mean(rmse_mc), np.std(rmse_mc)
-    # time_mc_mean, time_mc_std = np.mean(train_times_mc), np.std(train_times_mc)
-    #
-    # nll_pbp_mean, nll_pbp_std = np.mean(nll_pbp), np.std(nll_pbp)
-    # rmse_pbp_mean, rmse_pbp_std = np.mean(rmse_pbp), np.std(rmse_pbp)
-    # time_pbp_mean, time_pbp_std = np.mean(train_times_pbp), np.std(train_times_pbp)
-
-    nll_kbnn_mean, nll_kbnn_std = np.mean(nll_kbnn), np.std(nll_kbnn)
-    rmse_kbnn_mean, rmse_kbnn_std = np.mean(rmse_kbnn), np.std(rmse_kbnn)
-    time_kbnn_mean, time_kbnn_std = np.mean(train_times_kbnn), np.std(train_times_kbnn)
-
+  
     pm = u"\u00B1"
 
-    # print(f"MC Dropout RMSE: {'%.3f'%rmse_mc_mean} {pm} {'%.3f'%rmse_mc_std}")
-    # print(f"MC Dropout total train time / s: {'%.3f'%time_mc_mean} {pm} {'%.3f'%time_mc_std}")
-    # print(f"PBP RMSE: {'%.3f' % rmse_pbp_mean} {pm} {'%.3f' % rmse_pbp_std}")
-    # print(f"PBP NLL: {'%.3f' % nll_pbp_mean} {pm} {'%.3f' % nll_pbp_std}")
-    # print(f"PBP total train time / s: {'%.3f'%time_pbp_mean} {pm} {'%.3f'%time_pbp_std}")
-    print(f"KBNN RMSE: {'%.3f'%rmse_kbnn_mean} {pm} {'%.3f'%rmse_kbnn_std}")
-    print(f"KBNN NLL: {'%.3f'%nll_kbnn_mean} {pm} {'%.3f'%nll_kbnn_std}")
-    print(f"KBNN total train time / s: {'%.3f'%time_kbnn_mean} {pm} {'%.3f'%time_kbnn_std}")
+
+    print(f" RMSE: {'%.3f'%rmse_mean} {pm} {'%.3f'%rmse_std}")
+    print(f" NLL: {'%.3f'%nll_mean} {pm} {'%.3f'%nll_std}")
+    print(f" total train time / s: {'%.3f'%timen_mean} {pm} {'%.3f'%time_std}")
 
     with open(os.path.join(dir_name, "config_and_hyperparams.json"), "w") as f:
         json.dump(config_and_hyperparams_dict, f, indent=4, sort_keys=True)
@@ -106,9 +82,9 @@ def run_regression(ds, size, epochs, trials, hidden):
     fun = lambda u: u ** 3
     # plot_synthetic_plot(bnn,bnn_dropout,pbp  X_train, y_train, fun, dir_name, x_scaler=X_train_scaler, y_scaler=y_train_scaler)
     plot_synthetic_plot(bnn, X_train, y_train, fun, dir_name, x_scaler=X_train_scaler, y_scaler=y_train_scaler)
-    # eval = np.stack([rmse_mc, rmse_kbnn, train_times_kbnn]).T
-    eval = np.stack([ rmse_kbnn, train_times_kbnn]).T
-    np.savetxt(os.path.join(dir_name, "results.csv"), eval, delimiter=',', header="rmse_mc,rmse_kbnn,train_time")
+ 
+    eval = np.stack([ rmse, train_times]).T
+    np.savetxt(os.path.join(dir_name, "results.csv"), eval, delimiter=',', header="rmse,train_time")
 
 
 if __name__ == "__main__":
